4552638faffaad6a1bb9313660ee222e49be5894
DeepaliP98 <deepali846@gmail.com>
2023-09-05 12:45:22 +0200

    Adding a script to analyze stochastic runs

--- simulators/simulator7/example_cp_output_to_simulator_7_stochastic.py
+++ simulators/simulator7/example_cp_output_to_simulator_7_stochastic.py
@@ -19,8 +19,8 @@ printing = True
 # Read CP output and convert
 instance_name = f"{instance_size}_{instance_id}_factory_1"
 file_name = instance_name if cp_output == 'feasible' else f"{instance_name}_infeasible"
-cp_output = pd.read_csv(f"results/cp_model/{file_name}.csv", delimiter=";")
-print(f'Makespan according to CP outout is {max(cp_output["End"].tolist())}')
+cp_output = pd.read_csv(f"results/cp_model/development/instances_type_2/start times {file_name}.csv", delimiter=",")
+print(f'Makespan according to CP outout is {max(cp_output["end"].tolist())}')
 earliest_start = cp_output.to_dict('records')
 
 
@@ -28,7 +28,7 @@ evaluation = []
 for seed in scenario_seeds:
     # Read input instance
     my_productionplan = ProductionPlan(
-        **json.load(open('factory_data/instances_legacy/stochastic/json_instances/instance_' + instance_name + '.json')))
+        **json.load(open('factory_data/development/instances_type_2/instance_' + instance_name + '.json')))
     my_productionplan.set_earliest_start_times(earliest_start)
     my_productionplan.set_sequence(sequence=np.arange(instance_size))
     scenario_1 = my_productionplan.create_scenario(seed)
@@ -41,6 +41,7 @@ for seed in scenario_seeds:
     makespan, lateness, nr_unfinished = my_simulator.simulate(sim_time=2000, write=False, output_location=f""
     f"simulators/simulator7/outputs/example_cp_output_to_simulator.csv")
     if printing:
+        print(f'Run simulation for a scenario for instance {instance_name}')
         print(f'According to the simulation, the makespan is {makespan} and the lateness is {lateness}')
         print(f'The number of unfinished products {nr_unfinished}')
         print(f'The number of clashes (i.e. activities that could not be processed) is {my_simulator.nr_clashes}')
