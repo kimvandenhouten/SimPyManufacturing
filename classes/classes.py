0e14623f9e04ad8cb8e6aa9d2c4fe54adc7f86c5
kimvandenhouten <kimvandenhouten97@live.nl>
2023-10-05 14:56:02 +0200

    First start remove node in STN class. Indexing should be adjusted such that Floyd Warshall still runs

--- classes/classes.py
+++ classes/classes.py
@@ -434,8 +434,8 @@ class STN:
                 else:
                     # Possibly add function to distribution to convert distribution to uncertainty set
 
-                    lower_bound = max(round(activity.distribution.mean - 3 * activity.distribution.variance), 0)
-                    upper_bound = round(activity.distribution.mean + 6 * activity.distribution.variance)
+                    lower_bound = max(round(activity.distribution.mean - 5 * activity.distribution.variance), 0)
+                    upper_bound = round(activity.distribution.mean + 5 * activity.distribution.variance)
                     stn.add_interval_constraint(a_start, a_finish, lower_bound, upper_bound)
 
             # For every temporal relation in this product's temporal_relations, add edge between nodes with min and max lag
@@ -472,6 +472,14 @@ class STN:
     Floyd-Warshall algorithm
     Compute a matrix of shortest-path weights (if the graph contains no negative cycles)
     '''
+    def remove_node(self, node_idx):
+        # Remove a certain node and corresponding edges from the dictionairy
+        self.nodes.remove(node_idx)
+        self.edges.pop(node_idx)
+        for key in self.edges:
+            self.edges[key].pop(node_idx, None)
+
+        # TODO: should we adjust the indexing?
 
     def floyd_warshall(self):
         # Compute shortest distance graph path for this graph
