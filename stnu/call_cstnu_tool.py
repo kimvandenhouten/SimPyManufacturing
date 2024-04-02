ebeece992b8fa53550a0969144ced1a1799ed151
Léon Planken <l.r.planken@tudelft.nl>
2024-04-02 15:50:26 +0200

    Call Morris14 in Java code
    This also adds some checks whether files exist, and if the Java code gives error output, prints it

--- stnu/call_cstnu_tool.py
+++ stnu/call_cstnu_tool.py
@@ -1,24 +1,42 @@
+import os
 import subprocess
 
-#JAR_LOCATION = "/home/leon/Projects/CstnuTool-4.12/CSTNU-Tool-4.12.jar"
-#INSTANCE_LOCATION = "/home/leon/Projects/CstnuTool-4.12/Instances/AI4B.io/example_morris14_paper.stnu"
+JAR_LOCATION = None
 
-for (file_name, dc) in [("output_hunsberger23.stnu", True), ("output_morris14.stnu", False), ("output_slide118.stnu", True)]:
+if os.path.exists("/Users/kimvandenhouten"):
     JAR_LOCATION = "/Users/kimvandenhouten/Documents/PhD/Repositories/CstnuTool-4.12/CSTNU-Tool-4.12.jar"
-    INSTANCE_LOCATION = f"/Users/kimvandenhouten/Documents/PhD/Repositories/CstnuTool-4.12/Instances/AI4b.io/{file_name}"
-    # FIXME: can't we read directly from this repo ?
-    #INSTANCE_LOCATION = f"Users/kimvandenhouten/SimPyManufacturing/stnu/java_comparison/xml_files/{file_name}"
+elif os.path.exists("/home/leon"):
+    JAR_LOCATION = "/home/leon/Projects/CstnuTool-4.12-ai4b.io/CSTNU-Tool-4.12-ai4b.io.jar"
+
+if not JAR_LOCATION or not os.path.exists(JAR_LOCATION):
+    raise Exception("Could not find CSTNUTool-4.12-ai4b")
+
+FILE_LIST = [
+    ("input_hunsberger23.stnu", True),
+    ("input_morris14.stnu", False),
+    ("input_slide118.stnu", True)
+]
+
+for (file_name, dc) in FILE_LIST:
+    INSTANCE_LOCATION = os.path.abspath(f"stnu/java_comparison/xml_files/{file_name}")
+    if not os.path.exists(INSTANCE_LOCATION):
+        print(f"warning: could not find {INSTANCE_LOCATION}")
+        continue
+    print(f"running {INSTANCE_LOCATION}")
 
     cmd = [
-        'java',
-        '-cp',
-        JAR_LOCATION,
-        'it.univr.di.cstnu.algorithms.STNU',
-        INSTANCE_LOCATION,
+        'java', '-cp', JAR_LOCATION,
+        'it.univr.di.cstnu.algorithms.STNU', '-a', 'Morris2014', INSTANCE_LOCATION,
     ]
     if dc:
         print(f'Network should be DC')
     else:
         print(f'Network should not be DC')
+
     res = subprocess.run(cmd, capture_output=True, text=True)
+
+    if res.stderr:
+        print("ERROR")
+        print(res.stderr)
+
     print(res.stdout)
