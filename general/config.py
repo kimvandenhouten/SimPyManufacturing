025709c59cc9e26f74fe0bcef77366ec3d673e4e
Léon Planken <l.r.planken@tudelft.nl>
2024-06-24 14:54:56 +0200

    Introduce pyproject.default.toml
    We remove pyproject.toml from the repo and introduce a default version of the file.
    On first run, the Config class copies the default file to the actual configuration file. Any further changes to the configuration are ignored through .gitignore.

--- general/config.py
+++ general/config.py
@@ -1,13 +1,23 @@
+import os.path
+
 import tomli as tomllib
+
+PYPROJECT = "pyproject.toml"
+PYPROJECT_DEFAULT = "pyproject.default.toml"
 
 
 class Config:
     config = None
+    if not os.path.exists(PYPROJECT) and os.path.exists(PYPROJECT_DEFAULT):
+        import shutil
+        print(f"copying {PYPROJECT_DEFAULT} to {PYPROJECT} for first run")
+        shutil.copyfile(PYPROJECT_DEFAULT, PYPROJECT)
+
     try:
-        with open("pyproject.toml", "rb") as f:
+        with open(PYPROJECT, "rb") as f:
             config = tomllib.load(f)
     except FileNotFoundError:
-        raise FileNotFoundError("pyproject.toml not found")
+        raise FileNotFoundError(f"{PYPROJECT} not found")
 
     @classmethod
     def get(cls, *path):
